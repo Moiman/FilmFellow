c20f80ee384b42f873a3286a83ec54dea43956a0
Harri Nieminen <harri.nieminen@posteo.fi>
2024-03-12 09:56:13 +0200

    backend: add eslint and prettier

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,8 +3,8 @@ import cors from "cors";
 
 const app = express();
 
-app.use(cors({ credentials: true, origin: true }));
+app.use(cors({credentials: true, origin: true}));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 
 export default app;

4df3b6434ac55fa694561d05cc27a3603f53b92e
Jambo258 <74046093+Jambo258@users.noreply.github.com>
2024-03-18 15:01:29 +0200

    GitHub pipeline backend lint and tests (#12)
    * pipeline
    
    * testing
    
    * fixes on formatting certain files
    
    * pipeline fix

--- backend/__tests__/auth.test.ts
+++ backend/__tests__/auth.test.ts
@@ -4,11 +4,7 @@ import app from "../src/server.js";
 
 describe("auth route", () => {
   it("/auth/register without values", async () => {
-
-  const response = await request(app)
-    .post("/auth/register")
-    .send({});
-  expect(response.statusCode).toBe(400);
+    const response = await request(app).post("/auth/register").send({});
+    expect(response.statusCode).toBe(400);
   });
-
-})
+});

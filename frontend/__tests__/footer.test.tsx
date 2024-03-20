5399a2374b0b4c7d8449a3a3aabc5659bfbca8db
Katariina Ruotsalainen <bkruotsalainen@gmail.com>
2024-03-20 13:22:57 +0200

    Small test changes

--- frontend/__tests__/footer.test.tsx
+++ frontend/__tests__/footer.test.tsx
@@ -2,6 +2,8 @@ import { afterAll, beforeAll, expect, it } from "vitest";
 import { cleanup, render, screen } from "@testing-library/react";
 import { Footer } from "../components/footer";
 
+const githubLink = { text: "GitHub", href: "https://github.com/Moiman/FilmFellow/" };
+
 beforeAll(() => {
   render(<Footer />);
 });
@@ -10,18 +12,17 @@ afterAll(() => {
   cleanup();
 });
 
-const githubLink = { text: "GitHub", href: "https://github.com/Moiman/FilmFellow/" };
-const linkElement = screen.getByText(githubLink.text);
-
 it("checks if footer is rendered", () => {
   expect(Footer).toBeDefined();
 });
 
 it("checks if link to project GitHub exists", () => {
+  const linkElement = screen.getByText(githubLink.text);
   expect(linkElement).not.toBeNull();
   expect(linkElement).toBeDefined();
 });
 
 it("checks if all links have correct href attribute", () => {
+  const linkElement = screen.getByText(githubLink.text);
   expect(linkElement.getAttribute("href")).toBe(githubLink.href);
 });

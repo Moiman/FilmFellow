dde0a0c1d9bfb4a2b6407bf81cc6e13db4fdc83a
Jambo258 <jambomyllymaa@gmail.com>
2024-03-20 17:26:05 +0200

    minor fix

--- backend/src/routes/movieRouter.ts
+++ backend/src/routes/movieRouter.ts
@@ -41,7 +41,7 @@ movieRouter.get("/:movieId/reviews", async (req, res, next) => {
     const movieId = parseInt(req.params.movieId);
     console.log(movieId);
     const result = await getMovieReviewsById(movieId);
-    if (!result) {
+    if (result.length === 0) {
       return res.status(404).json({ error: `movie not found with id ${movieId}` });
     }
     return res.status(200).json(result);

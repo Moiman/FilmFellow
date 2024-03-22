cd64bd9dfc0cf5d612df753448f15577b14966ee
Jambo258 <jambomyllymaa@gmail.com>
2024-03-22 12:48:29 +0200

    readability changes to tests

--- frontend/cypress/e2e/routes.cy.ts
+++ frontend/cypress/e2e/routes.cy.ts
@@ -1,13 +1,18 @@
+const gitHubHref = "https://github.com/Moiman/FilmFellow/";
 describe("Routes tests", () => {
-  const gitHubHref = "https://github.com/Moiman/FilmFellow/";
-  it("visit default page", () => {
+
+  it("Visit default page", () => {
     cy.visit("/");
     cy.url().should("eq", Cypress.env("baseUrl") + "/");
     cy.get("h1").contains("FilmFellow");
   });
-  it("try to visit 404 page", () => {
+
+  it("Try to visit 404 page", () => {
     cy.request({ url: "/randompage", failOnStatusCode: false }).its("status").should("equal", 404);
   });
+});
+describe("Navbar tests", () => {
+
   it("Navigate to defaultpage with navbar", () => {
     cy.visit("/new");
     cy.url().should("eq", Cypress.env("baseUrl") + "/new");
@@ -15,6 +20,7 @@ describe("Routes tests", () => {
     cy.url().should("include", "/");
     cy.get("h1").contains("FilmFellow");
   });
+
   it("Navigate to new with navbar", () => {
     cy.visit("/");
     cy.url().should("eq", Cypress.env("baseUrl") + "/");
@@ -22,6 +28,7 @@ describe("Routes tests", () => {
     cy.url().should("include", "/new");
     cy.get("h2").contains("New");
   });
+
   it("Navigate to popular with navbar", () => {
     cy.visit("/");
     cy.url().should("eq", Cypress.env("baseUrl") + "/");
@@ -29,6 +36,7 @@ describe("Routes tests", () => {
     cy.url().should("include", "/popular");
     cy.get("h2").contains("Popular");
   });
+
   it("Navigate to bestrated with navbar", () => {
     cy.visit("/");
     cy.url().should("eq", Cypress.env("baseUrl") + "/");
@@ -36,6 +44,7 @@ describe("Routes tests", () => {
     cy.url().should("include", "/bestrated");
     cy.get("h2").contains("Best Rated");
   });
+
   it("Navigate to login with navbar", () => {
     cy.visit("/");
     cy.url().should("eq", Cypress.env("baseUrl") + "/");
@@ -43,11 +52,15 @@ describe("Routes tests", () => {
     cy.url().should("include", "/login");
     cy.get("h2").contains("Login");
   });
+
   it("Test navbar input field", () => {
     cy.visit("/");
     cy.get('input[placeholder="Search..."]').type("Jaws");
     cy.get('[data-cy="search-input"]').should("have.value", "Jaws");
   });
+});
+describe("Footer tests", () => {
+
   it("Test footer github link", () => {
     cy.visit("/");
     cy.get(".content").find(".github-link").should("have.attr", "href", gitHubHref);

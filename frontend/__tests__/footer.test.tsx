d0b5b610229f5440fc8bd9133c4392eb404e2e63
Katariina Ruotsalainen <64400810+bkruotsalainen@users.noreply.github.com>
2024-03-25 15:33:51 +0200

    Frontend: Section component (#19)
    * Fix button and main styling
    
    * Make typography more coherent
    
    * Add Section component and tests for header and content rendering
    
    * Add more visible focus for buttons
    
    * Fix test names

--- frontend/__tests__/footer.test.tsx
+++ frontend/__tests__/footer.test.tsx
@@ -1,4 +1,4 @@
-import { afterAll, beforeAll, expect, it } from "vitest";
+import { afterAll, beforeAll, expect, test } from "vitest";
 import { cleanup, render, screen } from "@testing-library/react";
 import { Footer } from "@/components/footer";
 
@@ -12,17 +12,17 @@ afterAll(() => {
   cleanup();
 });
 
-it("checks if footer is rendered", () => {
+test("Checks if footer is rendered", () => {
   expect(Footer).toBeDefined();
 });
 
-it("checks if link to project GitHub exists", () => {
+test("Checks if link to project GitHub exists", () => {
   const linkElement = screen.getByText(githubLink.text);
   expect(linkElement).not.toBeNull();
   expect(linkElement).toBeDefined();
 });
 
-it("checks if all links have correct href attribute", () => {
+test("Checks if all links have correct href attribute", () => {
   const linkElement = screen.getByText(githubLink.text);
   expect(linkElement.getAttribute("href")).toBe(githubLink.href);
 });

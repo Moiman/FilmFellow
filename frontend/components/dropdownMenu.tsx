991194f7226aded479034ac5639ab9d285bf7d43
Katariina Ruotsalainen <bkruotsalainen@gmail.com>
2024-03-27 09:56:15 +0200

    Add align for custom buttons

--- frontend/components/dropdownMenu.tsx
+++ frontend/components/dropdownMenu.tsx
@@ -2,27 +2,38 @@
 import { ReactElement, useState } from "react";
 import { Check, ChevronDown, ChevronUp } from "react-feather";
 
+/* Add more optional items if needed, id and name are only mandatory ones? */
 export type dropdownMenuItem = {
   id: number | string;
   name: string;
+  href?: string;
 };
 
-// PROPS:
-// options: Array of items (dropdownMenuItem) to be used in dropdown menu
-// onSelect: Function that takes selected item back to the parent and handles it there e.g. with UseState
+type buttonAlign = "center" | "right" | "left";
 
-// defaultOption: Optional default item that is already selected (e.g. "All")
-// button: Optional ReactElement that can be used as button for the menu
-// maxWidth: Optional maximum width of the dropdown menu, without this width is 100%
-// zIndex: Optional z-index to ease future layout handling
-// showSelected: show checkmark on selected item
+/* 
+MANDATORY PROPS:
+options: Array of items (dropdownMenuItem) to be used in dropdown menu
+onSelect: Function that takes selected item back to the parent and handles it there e.g. with UseState
+
+OPTIONAL PROPS:
+defaultOption: Optional default item that is already selected (e.g. "All")
+button: Optional ReactElement that can be used as button for the menu
+buttonAlign: Optional align for custom button, default is left
+width: Optional width of the dropdown menu, without this width is 100% parent width
+maxHeight: Optional maximum height of the dropdown menu, without this height is as long as it's content
+zIndex: Optional z-index to ease future layout handling
+showSelected: show checkmark on selected item
+*/
 
 interface DropdownMenuProps {
   options: dropdownMenuItem[];
   onSelect: (item: dropdownMenuItem) => void;
   defaultOption?: dropdownMenuItem;
   button?: ReactElement;
-  maxWidth?: string | number;
+  buttonAlign?: buttonAlign;
+  width?: number;
+  height?: number;
   zIndex?: number;
   showSelected?: boolean;
 }
@@ -31,13 +42,15 @@ export const DropdownMenu = ({
   options,
   defaultOption,
   button,
-  maxWidth,
+  buttonAlign,
+  width,
+  height,
   zIndex,
   onSelect,
   showSelected,
 }: DropdownMenuProps) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  const [selected, setSelected] = useState<dropdownMenuItem | null>(defaultOption ? defaultOption : null);
+  const [selected, setSelected] = useState<dropdownMenuItem | null>(defaultOption ? defaultOption : options[0]);
 
   if (selected === null) {
     return null;
@@ -51,13 +64,18 @@ export const DropdownMenu = ({
   return (
     <div
       className="dropdown-menu"
-      onClick={() => setIsOpen(!isOpen)}
-      style={{ maxWidth: maxWidth ? maxWidth.toString() + "px" : "100%", zIndex: zIndex }}
+      style={{
+        zIndex: zIndex,
+        justifyContent: buttonAlign ? buttonAlign : "left",
+      }}
     >
       {button ? (
-        button
+        <div onClick={() => setIsOpen(!isOpen)}>{button}</div>
       ) : (
-        <button className="header">
+        <button
+          className="header"
+          onClick={() => setIsOpen(!isOpen)}
+        >
           {selected && selected.name}
           {!isOpen ? (
             <ChevronDown
@@ -74,7 +92,10 @@ export const DropdownMenu = ({
       )}
 
       {isOpen && (
-        <div className="menu">
+        <div
+          className="menu"
+          style={{ maxHeight: height ? height + "px" : "fit-content", minWidth: width ? width + "px" : "fit-content" }}
+        >
           {options.map((option: dropdownMenuItem) => (
             <button
               key={option.id}

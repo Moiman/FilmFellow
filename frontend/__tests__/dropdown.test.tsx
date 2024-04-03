cff498ea3ad708f4f359ca2db3d2a00de0c93b33
Katariina Ruotsalainen <64400810+bkruotsalainen@users.noreply.github.com>
2024-04-03 09:04:19 +0300

    Frontend: Dropdown component (#22)
    * Add dropdownMenu component and style
    
    * Polish dropdownMenu
    
    * Add checkmark for selected option when showSelected is true
    
    * Add align for custom buttons
    
    * Fix width
    
    * Simplify dropdownMenu and use it in header
    
    * Remove overflow from Section component css
    
    * Polish dropdownMenu
    
    * Remove useless CSS class
    
    * Adjust default styles
    
    * Fix comment and remove empty space
    
    * Rename dropdrownMenu to dropdown
    
    * Fix imports
    
    * Remove not used code
    
    * Update e2e tests for header dropdown menu
    
    * Add vitest tests for dropdown
    
    * Remove waitFor import from test
    
    * Change empty to undefined
    
    * Remove use client
    
    * Update comments
    
    * Remove any type
    
    * Remove any
    
    * Remove unnecessary function and types
    
    * Remove repeated test
    
    * Separate e2e tests to multiple files
    
    * Change Dropdown component comments to JSDoc
    
    * Rename height and add closing Dropdown when clicking something else
    
    * Add useRef and remove eventListeners
    
    * Add missing type and fix JSDoc

---
+++ frontend/__tests__/dropdown.test.tsx
@@ -0,0 +1,61 @@
+import { expect, test, describe, afterEach } from "vitest";
+import { render, fireEvent, cleanup } from "@testing-library/react";
+import { Dropdown } from "@/components/dropdown";
+
+afterEach(() => cleanup());
+
+const defaultHeaderText = "Choose one";
+
+describe("Dropdown component", () => {
+  test("Renders with default props when optional props are not provided", () => {
+    const { getByText, queryByText } = render(
+      <Dropdown>
+        <div>Option 1</div>
+        <div>Option 2</div>
+        <div>Option 3</div>
+      </Dropdown>,
+    );
+
+    expect(getByText(defaultHeaderText)).toBeDefined();
+    expect(queryByText("Option 1")).toBeNull();
+
+    fireEvent.click(getByText(defaultHeaderText));
+    expect(getByText("Option 1")).toBeDefined();
+    expect(getByText("Option 2")).toBeDefined();
+    expect(getByText("Option 3")).toBeDefined();
+  });
+
+  test("Renders custom button correctly", () => {
+    const customButton = <button>Custom Button</button>;
+
+    const { getByText, queryByText } = render(
+      <Dropdown button={customButton}>
+        <div>Option 1</div>
+        <div>Option 2</div>
+        <div>Option 3</div>
+      </Dropdown>,
+    );
+
+    expect(queryByText(defaultHeaderText)).toBeNull();
+    expect(getByText("Custom Button")).toBeDefined();
+
+    fireEvent.click(getByText("Custom Button"));
+    expect(getByText("Option 1")).toBeDefined();
+  });
+
+  test("Dropdown opens and closes properly", async () => {
+    const { getByText, queryByText } = render(
+      <Dropdown>
+        <div>Option 1</div>
+        <div>Option 2</div>
+        <div>Option 3</div>
+      </Dropdown>,
+    );
+
+    fireEvent.click(getByText(defaultHeaderText));
+    expect(getByText("Option 1")).toBeDefined();
+
+    fireEvent.click(getByText(defaultHeaderText));
+    expect(queryByText("Option 1")).toBeNull();
+  });
+});
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,16 +1,80 @@
+"use client";
+import { Dropdown } from "@/components/dropdown";
 import { Section } from "@/components/section";
+import { useState } from "react";
+import { Check } from "react-feather";
 
 export default function Home() {
   const exampleHeader = (
     <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-      <h3>Component header</h3> <button>Button</button>
+      <h3>Component header</h3>
+      <Dropdown
+        button={<button>Test menu</button>}
+        width={200}
+        buttonAlign="right"
+      >
+        <button className="dropdown-item">1</button>
+        <button className="dropdown-item">2</button>
+        <button className="dropdown-item">3</button>
+      </Dropdown>
     </div>
   );
+
+  /* Placeholder data */
+  const exampleGenres = [
+    { id: 1, name: "All" },
+    { id: 23, name: "Drama" },
+    { id: 5, name: "Horror" },
+    { id: 2, name: "Sci-fi" },
+    { id: 16, name: "Western" },
+    { id: 9, name: "Action" },
+    { id: 12, name: "Romance" },
+    { id: 86, name: "Comedy" },
+    { id: 234, name: "Musical" },
+  ];
+
+  const [selectedGenre, setSelectedGenre] = useState(exampleGenres[0]);
 
   return (
     <main>
       <h1>FilmFellow</h1>
 
+      {/* Example of dropdown element */}
+      <div
+        style={{
+          marginBottom: "10px",
+          width: "100%",
+          display: "inline-flex",
+          justifyContent: "end",
+          alignItems: "center",
+          gap: "10px",
+        }}
+      >
+        <h6>Genre</h6>
+        <Dropdown
+          selected={selectedGenre ? selectedGenre.name : undefined}
+          width={200}
+          zIndex={10}
+        >
+          {exampleGenres.map(option => (
+            <button
+              key={option.id}
+              onClick={() => setSelectedGenre(option)}
+              className="dropdown-item"
+            >
+              {option.name}
+              {option.id === selectedGenre.id ? (
+                <Check
+                  size={20}
+                  color="#ffc700"
+                />
+              ) : null}
+            </button>
+          ))}
+        </Dropdown>
+      </div>
+
+      {/* Examples of sections */}
       <Section header={exampleHeader}>
         <p>This is a example usage of {"<Section>"} with component header.</p>
       </Section>

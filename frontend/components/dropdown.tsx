661dda97da1c925766c004a05b466facde29991c
Katariina Ruotsalainen <bkruotsalainen@gmail.com>
2024-04-03 08:51:10 +0300

    Add missing type and fix JSDoc

--- frontend/components/dropdown.tsx
+++ frontend/components/dropdown.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useEffect, useRef, useState } from "react";
+import { type ReactNode, useEffect, useRef, useState } from "react";
 import { ChevronDown, ChevronUp } from "react-feather";
 
 type buttonAlign = "center" | "right" | "left";
@@ -17,13 +17,14 @@ interface DropdownMenuProps {
  * This component can be used as a dropdown menu or select component. It works as a wrapper for links, options, etc.
  *
  * @component
- * @param {React.ReactNode} children - The children of the Dropdown component. Needs at least 1 child. You can use a button with the "dropdown-item" class for default styling or make a custom one.
- * @param {string} [selected] - Optional string for the header, for example, the currently selected option. Default is "Choose one".
- * @param {React.ReactNode} [button] - Optional ReactNode to replace the header. Use only Buttons for accessibility.
- * @param {"center"|"left"|"right"} [buttonAlign] - Optional alignment (center, right, left) for custom button. Default is "left".
- * @param {string} [width] - Optional width of the dropdown menu. Without this default width is 100% of parent width.
- * @param {string} [maxHeight] - Optional maximum height of the dropdown menu. Without this default height is "fit-content".
- * @param {number} [zIndex] - Optional z-index to ease future layout handling.
+ * @param {object} props
+ * @param {React.ReactNode} props.children - The children of the Dropdown component. Needs at least 1 child. You can use a button with the "dropdown-item" class for default styling or make a custom one.
+ * @param {string} props.[selected] - Optional string for the header, for example, the currently selected option. Default is "Choose one".
+ * @param {React.ReactNode} props.[button] - Optional ReactNode to replace the header. Use only Buttons for accessibility.
+ * @param {"center"|"left"|"right"} props.[buttonAlign] - Optional alignment (center, right, left) for custom button. Default is "left".
+ * @param {string} props.[width] - Optional width of the dropdown menu. Without this default width is 100% of parent width.
+ * @param {string} props.[maxHeight] - Optional maximum height of the dropdown menu. Without this default height is "fit-content".
+ * @param {number} props.[zIndex] - Optional z-index to ease future layout handling.
  * @returns {JSX.Element} A JSX element representing the Dropdown component.
  */
 

48789768ad75c38a05b45697049c78f60667ec09
Jambo258 <jambomyllymaa@gmail.com>
2024-04-04 09:09:44 +0300

    nextjs api tests

--- frontend/__tests__/mock.test.tsx
+++ frontend/__tests__/mock.test.tsx
@@ -1,21 +1,28 @@
 import { getServerSession } from "next-auth";
 import { vi } from "vitest";
-import {type Mock} from "vitest";
+import { type Mock } from "vitest";
 import { describe, expect, it } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { Header } from "@/components/header";
 import { SessionProvider } from "next-auth/react";
+import { createMocks, createRequest, createResponse } from "node-mocks-http";
+import { GET as getMovieById } from "@/app/api/movies/[id]/route";
+import { GET as getMovieReviewsById } from "@/app/api/movies/[id]/reviews/route";
+import { GET as getMoviesByQuery } from "@/app/api/movies/route";
+import { NextRequest, NextResponse } from "next/server";
 
+type ApiRequest = NextRequest & ReturnType<typeof createRequest>;
+type APiResponse = NextResponse & ReturnType<typeof createResponse>;
 
 vi.mock("next-auth", () => ({
   getServerSession: vi.fn(),
 }));
 
 describe("Mock Test", () => {
-  it("Logged in user", async () => {
+  it("Show sign out button when mocking session", async () => {
     const mockSession = {
       expires: "1",
-      user: { email: "testemail@gmail.com", username: "testuser", role: "guest", id: 1 },
+      user: { email: "testemail@gmail.com", username: "testuser", role: "user", id: 1 },
     };
 
     (getServerSession as Mock).mockReturnValueOnce([mockSession, false]);
@@ -25,8 +32,107 @@ describe("Mock Test", () => {
         <Header />
       </SessionProvider>,
     );
-      const button = screen.getByRole("button", { name: "Sign Out" });
-    expect(screen.getByRole("heading", {level: 4, name: "FilmFellow" })).toBeDefined();
+    const button = screen.getByRole("button", { name: "Sign Out" });
+    expect(screen.getByRole("heading", { level: 4, name: "FilmFellow" })).toBeDefined();
     expect(button).toBeDefined();
   });
-})
+});
+
+describe("/api/movies/id", () => {
+  it("Get movie by id successfully", async () => {
+    const params = { id: "278" };
+
+    const { req, res } = createMocks<ApiRequest, APiResponse>({
+      method: "GET",
+    });
+
+    const response = await getMovieById(req, { params });
+    expect(response.status).toBe(200);
+    const movie = await response.json();
+    expect(movie.id).toEqual(278)
+  });
+  it("Try to get movie by wrong id", async () => {
+    const params = { id: "123456789" };
+
+    const { req, res } = createMocks<ApiRequest, APiResponse>({
+      method: "GET",
+    });
+
+    const response = await getMovieById(req, { params });
+    expect(response.status).toBe(404);
+  });
+});
+
+describe("/api/movies/id/reviews", () => {
+  it("Get movie reviews by id successfully", async () => {
+    const params = { id: "278" };
+
+    const { req, res } = createMocks<ApiRequest, APiResponse>({
+      method: "GET",
+    });
+
+    const response = await getMovieReviewsById(req, { params });
+    expect(response.status).toBe(200);
+    const movieReviews = await response.json();
+    expect(movieReviews[0].movie.id).toEqual(278);
+    expect(movieReviews[0]).toHaveProperty("content");
+    expect(movieReviews[0]).toHaveProperty("author");
+  });
+  it("Try to get movie reviews by wrong id", async () => {
+    const params = { id: "123456789" };
+
+    const { req, res } = createMocks<ApiRequest, APiResponse>({
+      method: "GET",
+    });
+
+    const response = await getMovieReviewsById(req, { params });
+    expect(response.status).toBe(404);
+  });
+});
+
+describe("/api/movies/?limit&genre&type", () => {
+  it("/movies/?limit=10&type=new&genre=drama test movie endpoint for returning movies with limit 10, type from newest to oldest and genre drama", async () => {
+
+    const { req, res } = createMocks<ApiRequest, APiResponse>({
+      method: "GET",
+      url: "http://localhost:3000/api/movies?limit=5&genre=drama&type=new",
+      params: {
+        limit: "10",
+        type: "new",
+        genre: "drama"
+      },
+      query: {
+        limit: "10",
+        type: "new",
+        genre: "drama"
+      }
+
+    });
+    console.log(req)
+    const response = await getMoviesByQuery(req);
+    console.log(await response.json());
+    expect(response.status).toBe(200);
+    const moviesWithQuery = await response.json();
+    console.log(moviesWithQuery)
+  });
+  it("/movies/?limit=15&type=popular&genre=drama test movie endpoint for returning movies with limit 15, type most popular first and genre drama", async () => {
+    const params = { id: "123456789" };
+
+    const { req, res } = createMocks<ApiRequest, APiResponse>({
+      method: "GET",
+    });
+
+    const response = await getMoviesByQuery(req);
+    expect(response.status).toBe(404);
+  });
+  it("/movies/?limit=20&type=bestrated&genre=drama test movie endpoint for returning movies with limit 20, type highest rated first and genre drama", async () => {
+    const params = { id: "123456789" };
+
+    const { req, res } = createMocks<ApiRequest, APiResponse>({
+      method: "GET",
+    });
+
+    const response = await getMoviesByQuery(req);
+    expect(response.status).toBe(404);
+  });
+});
--- frontend/app/api/delete/[id]/route.ts
+++ frontend/app/api/delete/[id]/route.ts
@@ -1,4 +1,7 @@
+import { authOptions } from "@/authOptions";
 import { deleteUserById, findUserById } from "@/services/authService";
+import { Role } from "@prisma/client";
+import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 interface Params {
   id: string;
@@ -8,12 +11,26 @@ export async function DELETE(req: NextRequest, { params }: { params: Params }) {
   try {
     const userId = parseInt(params.id);
 
+    const session = await getServerSession(authOptions);
+    if (!session) {
+      return NextResponse.json(
+        { error: "Not Authorized" },
+        {
+          status: 400,
+        },
+      );
+    }
+
     const existingUser = await findUserById(userId);
     if (!existingUser) {
       return NextResponse.json({ error: "User not found" }, { status: 404 });
     }
-    const deletedUser = await deleteUserById(userId);
-    return NextResponse.json(deletedUser, { status: 200 });
+    if (session.user.id === userId || session.user.role === Role.admin) {
+      const deletedUser = await deleteUserById(userId);
+      return NextResponse.json(deletedUser, { status: 200 });
+    } else {
+      return NextResponse.json({ error: "Cant delete other users unless admin" }, { status: 400 });
+    }
   } catch (err) {
     return NextResponse.json({ error: err }, { status: 400 });
   }

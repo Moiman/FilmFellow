16e1be1449b7a4bf44a069503d3a106e3be5d9a4
Jambo258 <jambomyllymaa@gmail.com>
2024-04-05 13:58:10 +0300

    styling log in form

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,18 +1,44 @@
 "use client";
+import { yupResolver } from "@hookform/resolvers/yup";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { Eye, EyeOff } from "react-feather";
+import Link from "next/link";
+
+interface LoginFormData {
+  email: string;
+  password: string;
+}
+
+const loginUserSchema = yup.object({
+  email: yup.string().trim().required("Email is required").email("Must be a valid email"),
+  password: yup.string().required("Password is required").min(6, "Password must be at least 6 characters long"),
+});
 
 export default function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm<LoginFormData>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    resolver: yupResolver(loginUserSchema),
+  });
+
   const router = useRouter();
-  const onSubmit = async (e: any) => {
-    e.preventDefault();
+  const onSubmit = async (data: LoginFormData) => {
+    console.log(data);
     const credentials = {
-      email: email,
-      password: password,
+      email: data.email,
+      password: data.password,
     };
 
     const response = await signIn("login", {
@@ -30,28 +56,36 @@ export default function Login() {
     // console.log(response?.error);
   };
   return (
-    <main>
-      <h2>Login</h2>
-      <form onSubmit={onSubmit}>
+    <main className="loginForm">
+      <h2 className="loginText">Login</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="email">Email</label>
         <input
           type="email"
-          name="email"
-          onChange={e => setEmail(e.target.value)}
-          value={email}
-          required
+          {...register("email")}
         />
+        {errors?.email && <p className="error-text">{errors?.email?.message}</p>}
         <label htmlFor="password">Password</label>
         <input
-          required
-          onChange={e => setPassword(e.target.value)}
-          type="password"
-          name="password"
-          value={password}
+          type={showPassword ? "text" : "password"}
+          {...register("password")}
         />
-        <button type="submit">Submit</button>
+        <button onClick={() => setShowPassword(!showPassword)}>{showPassword ? <Eye /> : <EyeOff />}</button>
+        {errors?.password && <p className="error-text">{errors?.password?.message}</p>}
+        <button
+          className="login-submit"
+          type="submit"
+          disabled={isSubmitting}
+        >
+          Login
+        </button>
+        <div className="login-route-change">
+          <Link href="/">Cant login ?</Link>
+          <p> / </p>
+          <Link href="/register">Register</Link>
+        </div>
       </form>
-      {error}
+      <p className="error-text">{error}</p>
     </main>
   );
 }

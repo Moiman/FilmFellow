3c0aeef0c816ad2d0a60a52775d8c41f1fe083fe
Tyni <tommi.tyni@tuni.fi>
2024-04-08 09:02:15 +0300

    prettier

--- frontend/cypress/e2e/modal.cy.ts
+++ frontend/cypress/e2e/modal.cy.ts
@@ -8,13 +8,17 @@ describe("Modal tests", () => {
 
   it("close modal from X", () => {
     cy.visit(Cypress.env("baseUrl") + "/testing?showModal=y");
-    cy.get(".modal-title").within(() => { cy.get("a").click() });
+    cy.get(".modal-title").within(() => {
+      cy.get("a").click()
+    });
     cy.url().should("eq", Cypress.env("baseUrl") + "/testing");
   });
 
   it("close modal from btn", () => {
     cy.visit(Cypress.env("baseUrl") + "/testing?showModal=y");
-    cy.get(".modal-btn").within(() => { cy.get("a").click() });
+    cy.get(".modal-btn").within(() => {
+      cy.get("a").click()
+    });
     cy.url().should("eq", Cypress.env("baseUrl") + "/testing");
   })
 });

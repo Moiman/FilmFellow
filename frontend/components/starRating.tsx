3502d68bfa35f5867f7e21933e99b0023ef76803
Katariina Ruotsalainen <bkruotsalainen@gmail.com>
2024-04-11 14:41:07 +0300

    Simplify startRating for now

--- frontend/components/starRating.tsx
+++ frontend/components/starRating.tsx
@@ -6,26 +6,12 @@ interface StarRatingProps {
 }
 
 export const StarRating = ({ onChange }: StarRatingProps) => {
-  const [hoverRating, setHoverRating] = useState(0);
   const [rating, setRating] = useState(0);
-  const [previousRating, setPreviousRating] = useState(0);
-
-  const handleMouseOver = (hoveredRating: number) => {
-    setPreviousRating(rating);
-    setRating(0);
-    setHoverRating(hoveredRating);
-  };
-
-  const handleMouseOut = () => {
-    setRating(previousRating);
-    setHoverRating(0);
-  };
 
   const handleClick = (clickedRating: number) => {
     const newRating = clickedRating === rating ? 0 : clickedRating;
 
     setRating(newRating);
-    setPreviousRating(newRating);
     onChange(newRating);
   };
 
@@ -35,10 +21,8 @@ export const StarRating = ({ onChange }: StarRatingProps) => {
         <Star
           key={index}
           onClick={() => handleClick(index)}
-          onMouseOver={() => handleMouseOver(index)}
-          onMouseOut={handleMouseOut}
-          stroke={hoverRating >= index ? "#dea000" : rating >= index ? "#ffc700" : "#eff2f2"}
-          fill={hoverRating >= index ? "#dea000" : rating >= index ? "#ffc700" : "#eff2f2"}
+          stroke={rating >= index ? "#ffc700" : "#eff2f2"}
+          fill={rating >= index ? "#ffc700" : "#eff2f2"}
           strokeWidth={2}
           size={30}
           style={{ cursor: "pointer" }}

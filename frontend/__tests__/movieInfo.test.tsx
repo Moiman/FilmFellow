5b84e26faf5ab5344eecf4c8ad711b24fff95b0d
Katariina Ruotsalainen <bkruotsalainen@gmail.com>
2024-04-15 13:14:16 +0300

    Add tests for movieInfo

---
+++ frontend/__tests__/movieInfo.test.tsx
@@ -0,0 +1,48 @@
+import { expect, test, afterEach } from "vitest";
+import { cleanup, fireEvent, render } from "@testing-library/react";
+import { MovieInfo } from "@/components/movies/movieInfo";
+
+const mockedMovie = {
+  title: "Test Movie",
+  directors: ["Director 1", "Director 2"],
+  releaseYear: 2022,
+  ageRestrictions: "PG-13",
+  runtime: 150,
+  overview: "This is a test movie overview",
+  posterPath: "test_poster.jpg",
+  voteAverage: 7.55,
+};
+
+afterEach(() => cleanup());
+
+test("renders MovieInfo component with given movie data", () => {
+  const { getByText } = render(<MovieInfo movie={mockedMovie} />);
+
+  expect(getByText("Test Movie")).toBeDefined();
+  expect(getByText("Directed by Director 1, Director 2")).toBeDefined();
+  expect(getByText("2022")).toBeDefined();
+  expect(getByText("PG-13")).toBeDefined();
+  expect(getByText("2 h 30 min")).toBeDefined();
+  expect(getByText("This is a test movie overview")).toBeDefined();
+});
+
+test("handles button clicks correctly", () => {
+  const { getByText } = render(<MovieInfo movie={mockedMovie} />);
+  const markAsWatchedButton = getByText("Mark as watched");
+  const addToFavoritesButton = getByText("Add to favorites");
+  const addToWatchlistButton = getByText("Add to watchlist");
+
+  fireEvent.click(markAsWatchedButton);
+  fireEvent.click(addToFavoritesButton);
+  fireEvent.click(addToWatchlistButton);
+
+  expect(getByText("Remove from watched")).toBeDefined();
+  expect(getByText("Remove from favorites")).toBeDefined();
+  expect(getByText("Remove from watchlist")).toBeDefined();
+});
+
+test("renders voteAverage correctly", () => {
+  const { getByText } = render(<MovieInfo movie={mockedMovie} />);
+
+  expect(getByText("7.6")).toBeDefined();
+});

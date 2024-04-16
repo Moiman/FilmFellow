46ae50f4bb0e0b47ce13eb707cb07bb736de0033
Katariina Ruotsalainen <bkruotsalainen@gmail.com>
2024-04-16 15:27:23 +0300

    Add tabindex for login and register forms

--- frontend/app/login/form.tsx
+++ frontend/app/login/form.tsx
@@ -94,6 +94,7 @@ export default function Login() {
                 className="form-group-icon button-transparent"
                 type="button"
                 onClick={() => setShowPassword(!showPassword)}
+                tabIndex={-1}
               >
                 {showPassword ? <Eye size={20} /> : <EyeOff size={20} />}
               </button>

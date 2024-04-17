25663505aeea14ba6da67a937e03acf9fea0dc57
Jambo258 <jambomyllymaa@gmail.com>
2024-04-17 10:46:31 +0300

    new admin script and tests

---
+++ frontend/scripts/initAdmin.ts
@@ -0,0 +1,41 @@
+import argon2 from "argon2";
+import { PrismaClient } from "@prisma/client";
+import { Role } from "@prisma/client";
+
+const initAdmin = async () => {
+  try {
+    const { ADMIN_USERNAME, ADMIN_EMAIL, ADMIN_PASSWORD } = process.env;
+    if (!ADMIN_USERNAME || !ADMIN_EMAIL || !ADMIN_PASSWORD) {
+      console.log("Missing env variables");
+      return;
+    }
+    const admin = {
+      username: ADMIN_USERNAME,
+      email: ADMIN_EMAIL,
+      password: ADMIN_PASSWORD,
+      role: Role.admin,
+    };
+    const prisma = new PrismaClient();
+
+    const existingAdmin = await prisma.users.findFirst({
+      where: { OR: [{ email: { equals: admin.email } }, { username: { equals: admin.username } }] },
+    });
+
+    if (!existingAdmin) {
+      const createdAdmin = await prisma.users.create({
+        data: {
+          username: admin.username,
+          email: admin.email,
+          password: await argon2.hash(admin.password),
+          role: Role.admin,
+        },
+      });
+      console.log("Admin account", createdAdmin.username, createdAdmin.email, "created successfully");
+    } else {
+      console.error("username or email is already in use for admin", existingAdmin.username);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+initAdmin();

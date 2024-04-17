15d6a5373139b074c0c6ae7231c0d4e09da4f7d8
Jambo258 <jambomyllymaa@gmail.com>
2024-04-17 13:05:57 +0300

    fixed import

--- frontend/scripts/initAdmin.ts
+++ frontend/scripts/initAdmin.ts
@@ -1,6 +1,5 @@
 import argon2 from "argon2";
-import { PrismaClient } from "@prisma/client";
-import { Role } from "@prisma/client";
+import { PrismaClient, Role } from "@prisma/client";
 
 const initAdmin = async () => {
   try {

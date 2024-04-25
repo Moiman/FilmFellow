a5a21dcbd6774e5cbd662ca59302a7282ad9afa2
Tyni <tommi.tyni@tuni.fi>
2024-04-25 12:15:07 +0300

    cleaned up code and fixed typing

--- frontend/app/home/homeMovieList.tsx
+++ frontend/app/home/homeMovieList.tsx
@@ -5,26 +5,25 @@ import { Section } from "@/components/section";
 import Image from "next/image";
 
 import Link from "next/link";
-import {
-  AwaitedReactNode,
-  JSXElementConstructor,
-  ReactElement,
-  ReactNode,
-  ReactPortal,
-  useEffect,
-  useState,
-} from "react";
+import { JSX, SetStateAction, useEffect, useState } from "react";
 import { fetchGenres, fetchMovies } from "./movieFetches";
 
 export const MovieList = () => {
-  const [newMovieList, setNewMovieList] = useState<any>();
-  const [popularMovieList, setPopularMovieList] = useState<any>();
-  const [bestratedMovieList, setBestratedMovieList] = useState<any>();
+  const [newMovieList, setNewMovieList] = useState<JSX.Element[]>();
+  const [popularMovieList, setPopularMovieList] = useState<JSX.Element[]>();
+  const [bestratedMovieList, setBestratedMovieList] = useState<JSX.Element[]>();
   const [allgenres, setAllGenres] = useState<any>();
   const [selectedGenre, setSelectedGenre] = useState("action");
 
-  const getPosters = async (setMovieList: any, type: string, genre: string) => {
-    const moviesArr = await fetchMovies(6, type, genre);
+  const getPosters = async (
+    setMovieList: {
+      (value: SetStateAction<JSX.Element[] | undefined>): void;
+      (arg0: JSX.Element[]): void;
+    },
+    type: string,
+    genre: string,
+  ) => {
+    const moviesArr = await fetchMovies(7, type, genre);
     if (moviesArr) {
       const test = moviesArr.map((movie: { id: number; poster_path: string }) => {
         return (
@@ -69,7 +68,7 @@ export const MovieList = () => {
     getPosters(setNewMovieList, "new", selectedGenre);
     getPosters(setPopularMovieList, "popular", selectedGenre);
     getPosters(setBestratedMovieList, "bestrated", selectedGenre);
-  }, [allgenres, selectedGenre]);
+  }, [selectedGenre]);
 
   return (
     <div>
@@ -79,7 +78,7 @@ export const MovieList = () => {
       <Section
         header={
           <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-            <h3>{"title"}</h3>
+            <h3>{"New Movies"}</h3>
             <Link href={"/new"}>See all</Link>
           </div>
         }
@@ -89,7 +88,7 @@ export const MovieList = () => {
       <Section
         header={
           <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-            <h3>{"title"}</h3>
+            <h3>{"Popular Movies"}</h3>
             <Link href={"/new"}>See all</Link>
           </div>
         }
@@ -99,7 +98,7 @@ export const MovieList = () => {
       <Section
         header={
           <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-            <h3>{"title"}</h3>
+            <h3>{"Best Rated Movies"}</h3>
             <Link href={"/new"}>See all</Link>
           </div>
         }
--- frontend/app/home/page.tsx
+++ frontend/app/home/page.tsx
@@ -1,13 +1,6 @@
 import { MovieList } from "@/app/home/homeMovieList";
-import { fetchGenres, fetchMovies } from "./movieFetches";
-import { Dropdown } from "@/components/dropdown";
 
 export default async function home() {
-  const newMovies = await fetchMovies(6, "new", "action");
-  const popularMovies = await fetchMovies(6, "popular", "action");
-  const bestRatedMovies = await fetchMovies(6, "bestrated", "action");
-  const genres = await fetchGenres();
-
   return (
     <main>
       <MovieList />

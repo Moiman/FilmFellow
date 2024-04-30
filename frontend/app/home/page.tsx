6a16e55a4128516f7b24b38cb20017b6ead78a09
Harri Nieminen <harri.nieminen@posteo.fi>
2024-04-30 13:52:33 +0300

    cleanup

--- frontend/app/home/page.tsx
+++
@@ -1,9 +0,0 @@
-import { MovieList } from "@/app/home/homeMovieList";
-
-export default async function home({ searchParams }: { searchParams?: { genre: string } }) {
-  return (
-    <main>
-      <MovieList selectedGenre={searchParams?.genre} />
-    </main>
-  );
-}

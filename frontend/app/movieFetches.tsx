9b56da006efaea394ce85fbc02ae0a89f97e83da
Tommi Tyni <90600923+TTyni@users.noreply.github.com>
2024-05-02 11:01:23 +0300

    Visitor front page (#50)
    * frontpage plannings and some test data
    
    * end of day commit
    
    * poster path fetch from database
    
    * end of day commit
    
    * added get for all genres
    
    * end of day commit
    
    * sass
    
    * genre selection
    
    * cleaned up code and fixed typing
    
    * type fix
    
    * e2e tests
    
    * Dropdown styling
    
    cleaning up code
    
    dropdown styling
    
    * review fixes
    
    * poster size
    
    * make frontpage server component
    
    * default all movies for frontpage
    
    * add check for selected genre
    
    * simplify promise logic
    
    * cleanup
    
    * fix test
    
    * fix test
    
    * fix cypress tests
    
    * update tests
    
    * fix test
    
    * review style fixes
    
    * review css fixes
    
    * removed unnecessary files
    
    ---------
    
    Co-authored-by: Harri Nieminen <harri.nieminen@posteo.fi>

---
+++ frontend/app/movieFetches.tsx
@@ -0,0 +1,16 @@
+"use server";
+import { getAllGenres, getMovieByLimitTypeGenre } from "@/services/movieService";
+
+export const fetchMovies = async (limit: number, type: string, genre: string | undefined) => {
+  const movies = await getMovieByLimitTypeGenre(limit, type, genre);
+
+  const posters = movies?.map(movie => {
+    return { id: movie.id, poster_path: movie.poster_path };
+  });
+  return posters;
+};
+
+export const fetchGenres = async () => {
+  const genres = await getAllGenres();
+  return genres;
+};

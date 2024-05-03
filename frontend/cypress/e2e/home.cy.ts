beb7a776ae918c1162395e2d72fdd9295eae2aa5
Katariina Ruotsalainen <bkruotsalainen@gmail.com>
2024-05-03 15:37:55 +0300

    Fix tests

--- frontend/cypress/e2e/home.cy.ts
+++ frontend/cypress/e2e/home.cy.ts
@@ -3,20 +3,19 @@ describe("home page tests", () => {
     cy.visit("/");
     cy.location("pathname").should("eq", "/");
     cy.wait(500);
-    cy.get(".poster-list").should("be.visible");
     cy.get(".dropdown-header").click();
     cy.get(".dropdown-menu").should("be.visible");
     cy.get(".dropdown-menu").contains("War").click();
     cy.wait(1000);
     cy.get(".dropdown").contains("War");
-    cy.get(".poster-list").should("be.visible");
+    cy.get(".movie-list").should("be.visible");
   });
 
   it("test poster link", () => {
     cy.visit("/");
     cy.location("pathname").should("eq", "/");
     cy.wait(1000);
-    cy.get(".poster-list > a").eq(1).click();
+    cy.get(".movie-list > a").eq(1).click();
     cy.wait(500);
     cy.location("pathname").should("include", "/movies/");
   });

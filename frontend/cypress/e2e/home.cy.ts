f2fd2c74346a09d7ff194624e26052ca06a6e6a7
Katariina Ruotsalainen <bkruotsalainen@gmail.com>
2024-05-03 15:49:18 +0300

    Remove unnecessary waits

--- frontend/cypress/e2e/home.cy.ts
+++ frontend/cypress/e2e/home.cy.ts
@@ -2,11 +2,10 @@ describe("home page tests", () => {
   it("basic home page tests", () => {
     cy.visit("/");
     cy.location("pathname").should("eq", "/");
-    cy.wait(500);
+    cy.get(".movie-list").should("be.visible");
     cy.get(".dropdown-header").click();
     cy.get(".dropdown-menu").should("be.visible");
     cy.get(".dropdown-menu").contains("War").click();
-    cy.wait(1000);
     cy.get(".dropdown").contains("War");
     cy.get(".movie-list").should("be.visible");
   });
@@ -14,16 +13,13 @@ describe("home page tests", () => {
   it("test poster link", () => {
     cy.visit("/");
     cy.location("pathname").should("eq", "/");
-    cy.wait(1000);
     cy.get(".movie-list > a").eq(1).click();
-    cy.wait(500);
     cy.location("pathname").should("include", "/movies/");
   });
 
   it("test see all link", () => {
     cy.visit("/");
     cy.location("pathname").should("eq", "/");
-    cy.wait(1000);
     cy.get("a").contains("See all").click();
     cy.location("pathname").should("eq", "/new");
   });

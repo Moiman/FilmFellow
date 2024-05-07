f90fd6f2488d74f8069a1231c0307a1b76f25ed0
TTyni <tommityni@msn.com>
2024-05-07 13:58:33 +0300

    removed unnecessary ?

--- frontend/app/movieFetches.tsx
+++ frontend/app/movieFetches.tsx
@@ -4,7 +4,7 @@ import { getAllGenres, getMovieByLimitTypeGenre } from "@/services/movieService"
 export const fetchMovies = async (limit: number, type: string, genre: string | undefined) => {
   const movies = await getMovieByLimitTypeGenre(limit, type, genre);
 
-  const posters = movies?.map(movie => {
+  const posters = movies.map(movie => {
     return { id: movie.id, poster_path: movie.poster_path, title: movie.title };
   });
   return posters;

31cd701aef4f1cf5a2434f35e5310ce2ff82c37f
Katariina Ruotsalainen <bkruotsalainen@gmail.com>
2024-05-07 14:52:23 +0300

    Fix menu padding

--- frontend/app/admin/layout.tsx
+++ frontend/app/admin/layout.tsx
@@ -15,21 +15,18 @@ export default function SideBarLayout({ children }: Props) {
         <div className="admin-panel-side-bar">
           <Link
             href="/admin/users"
-            style={{ width: "100px", padding: "10px" }}
             className={currentPath === "/admin/users" ? "active-admin-link" : ""}
           >
             Users
           </Link>
           <Link
             href="/admin/reports"
-            style={{ width: "100px", padding: "10px" }}
             className={currentPath === "/admin/reports" ? "active-admin-link" : ""}
           >
             Reports
           </Link>
           <Link
             href="/admin/statistics"
-            style={{ width: "100px", padding: "10px" }}
             className={currentPath === "/admin/statistics" ? "active-admin-link" : ""}
           >
             Statistics

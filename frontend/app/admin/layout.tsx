518dfebfd346260f576eba274fdbc91b3660cfde
Katariina Ruotsalainen <bkruotsalainen@gmail.com>
2024-05-07 12:33:09 +0300

    Add different active link colors

--- frontend/app/admin/layout.tsx
+++ frontend/app/admin/layout.tsx
@@ -16,21 +16,21 @@ export default function SideBarLayout({ children }: Props) {
           <Link
             href="/admin/users"
             style={{ width: "100px", padding: "10px" }}
-            className={currentPath === "/admin/users" ? "admin-panel-active-link" : ""}
+            className={currentPath === "/admin/users" ? "active-admin-link" : ""}
           >
             Users
           </Link>
           <Link
             href="/admin/reports"
             style={{ width: "100px", padding: "10px" }}
-            className={currentPath === "/admin/reports" ? "admin-panel-active-link" : ""}
+            className={currentPath === "/admin/reports" ? "active-admin-link" : ""}
           >
             Reports
           </Link>
           <Link
             href="/admin/statistics"
             style={{ width: "100px", padding: "10px" }}
-            className={currentPath === "/admin/statistics" ? "admin-panel-active-link" : ""}
+            className={currentPath === "/admin/statistics" ? "active-admin-link" : ""}
           >
             Statistics
           </Link>

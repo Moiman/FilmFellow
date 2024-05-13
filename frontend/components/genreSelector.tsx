1c0ed7b578de2301ea537a6f5f2e8cce922049cb
Katariina Ruotsalainen <bkruotsalainen@gmail.com>
2024-05-13 13:51:45 +0300

    Change genreSelector and header dropdown max heights

--- frontend/components/genreSelector.tsx
+++ frontend/components/genreSelector.tsx
@@ -48,7 +48,7 @@ export default async function GenreSelector({ selectedGenre }: { selectedGenre: 
         zIndex={10}
         width={200}
         selected={selectedGenre ?? "All"}
-        maxHeight={300}
+        maxHeight={400}
       >
         {await getGenres()}
       </Dropdown>

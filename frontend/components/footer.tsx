f3d6b82650f25367bea3c0b421e2c39e93d6428e
Katariina Ruotsalainen <bkruotsalainen@gmail.com>
2024-05-15 10:03:20 +0300

    Remove testing page and add tmdb logo to footer

--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import { GitHub } from "react-feather";
 
 export const Footer = () => {
@@ -9,7 +10,16 @@ export const Footer = () => {
           href="https://github.com/Moiman/FilmFellow/"
           className="github-link"
         >
-          <GitHub size={18} /> GitHub
+          <GitHub size={20} />
+        </Link>
+
+        <Link href="https://www.themoviedb.org/">
+          <Image
+            src="/logos/tmdb_logo.svg"
+            height={32}
+            width={32}
+            alt="The Movie Database"
+          />
         </Link>
       </div>
     </footer>

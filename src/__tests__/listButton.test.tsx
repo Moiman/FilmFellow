03f3517f74e6a3e2f3aceca6b945bc8baf5fa8d3
Harri Nieminen <moiman@posteo.net>
2024-05-16 14:23:33 +0300

    fix test

--- src/__tests__/listButton.test.tsx
+++ src/__tests__/listButton.test.tsx
@@ -2,37 +2,34 @@ import { cleanup, render, screen } from "@testing-library/react";
 import { expect, test, describe, beforeEach, afterEach } from "vitest";
 
 import { ListButton } from "@/app/users/[id]/listButton";
+import type { getUserLists } from "@/services/listService";
 
-const userId = 1;
-const list = {
+type ListButtonProps = Awaited<ReturnType<typeof getUserLists>>[0];
+
+const list: ListButtonProps = {
   id: 1,
   name: "Test List",
-  movies: [
-    { id: 1, title: "Movie 1" },
-    { id: 2, title: "Movie 2" },
-  ],
+  listMovies: [{ movie: { poster_path: "Movie 1" } }, { movie: { poster_path: "Movie 2" } }],
+  _count: {
+    listMovies: 2,
+  },
 };
 
-beforeEach(() =>
-  render(
-    <ListButton
-      userId={userId}
-      list={list}
-    />,
-  ),
-);
+beforeEach(() => {
+  render(<ListButton list={list} />);
+});
 
 afterEach(() => cleanup());
 
 describe("ListButton", () => {
   test("renders list name and movie count", () => {
     expect(screen.getByText(list.name)).toBeDefined();
-    expect(screen.getByText(list.movies.length.toString())).toBeDefined();
+    expect(screen.getByText(list.listMovies.length.toString())).toBeDefined();
   });
 
   test("list have right href", () => {
     const linkElement = screen.getByRole("link");
-    const expectedHref = `/users/${userId}/lists/${list.id}`;
+    const expectedHref = `/lists/${list.id}`;
     const href = linkElement.getAttribute("href");
     expect(href).toBe(expectedHref);
   });

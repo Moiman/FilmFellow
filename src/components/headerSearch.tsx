3faa72b093c8ae4f6058a0d871e19138631135a3
Katariina Ruotsalainen <bkruotsalainen@gmail.com>
2024-05-17 11:05:32 +0300

    Add closing search when clicking outside of it

--- src/components/headerSearch.tsx
+++ src/components/headerSearch.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { Search } from "react-feather";
@@ -16,6 +16,7 @@ type SearchResult = {
 
 // PLACEHOLDER LOGIC: Currently returns movies that includes searched word in title
 // Searches at maximum every 1 seconds so search isn't spammed on every input change
+// "See all results" takes currently nowhere as there is no search page yet
 
 export const HeaderSearch = () => {
   const [search, setSearch] = useState<string>("");
@@ -23,12 +24,12 @@ export const HeaderSearch = () => {
   const [loading, setLoading] = useState<boolean>(false);
 
   const router = useRouter();
+  const searchBarRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const searchDelay = setTimeout(() => {
       const getMovies = async () => {
         if (search.trim() === "") {
-          setResults([]);
           setLoading(false);
           return;
         }
@@ -39,21 +40,39 @@ export const HeaderSearch = () => {
 
       getMovies();
     }, 1000);
-
     return () => clearTimeout(searchDelay);
   }, [search]);
 
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (searchBarRef.current && !searchBarRef.current.contains(event.target as Node)) {
+        setSearch("");
+        setResults([]);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  });
+
   return (
-    <div className="header-searchbar">
+    <div
+      className="header-searchbar"
+      ref={searchBarRef}
+    >
       <input
         type="text"
         data-cy="search-input"
         className="searchbar-input"
-        placeholder="Search..."
+        placeholder="Search for a movie..."
         value={search}
         onChange={e => {
           setSearch(e.target.value);
           setLoading(true);
+          setResults([]);
         }}
       />
       <button className="button-transparent">
@@ -65,6 +84,7 @@ export const HeaderSearch = () => {
 
       {search.length > 0 && (
         <div className="searchbar-results">
+          {loading && <p className="searchbar-searching">Searching for results...</p>}
           {results.map(movie => (
             <button
               className="searchbar-movie-result"
@@ -87,7 +107,6 @@ export const HeaderSearch = () => {
             </button>
           ))}
 
-          {loading && <p className="searchbar-searching">Searching for results...</p>}
           {!loading && results.length === 0 && <p className="searchbar-searching">No results found.</p>}
 
           <button

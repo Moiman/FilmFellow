de7fa24aa9bb054775df1be4a96a5ef07aefc050
Katariina Ruotsalainen <bkruotsalainen@gmail.com>
2024-05-20 15:22:40 +0300

    Add layout for list page where styles are returned as children

--- src/app/lists/[listId]/catalogue/page.tsx
+++ src/app/lists/[listId]/catalogue/page.tsx
@@ -1,13 +1,7 @@
-import Link from "next/link";
 import { notFound } from "next/navigation";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/authOptions";
-import { List, Grid, Table } from "react-feather";
-
-import { Section } from "@/components/section";
 import { getList } from "@/services/listService";
-import { DeleteList } from "../deleteList";
-import { RenameList } from "../renameList";
 import { MovieCatalogue } from "../movieCatalogue";
 
 export default async function CataloguePage({ params }: { params: { listId: string } }) {
@@ -22,45 +16,5 @@ export default async function CataloguePage({ params }: { params: { listId: stri
 
   const movies = list.listMovies.map(movie => movie.movie);
 
-  return (
-    <main className="list">
-      <Section
-        header={
-          <div className="header">
-            <div className="list-title">
-              <h2 className="yellow-name-header h3">
-                <Link href={"/users/" + list.userId}>{list.user.username}</Link>&rsquo;s {list.name}
-              </h2>
-              <p className="movie-amount">{movies.length}</p>
-            </div>
-
-            {session && session.user.id === list.userId && (
-              <div className="list-edit">
-                <RenameList id={id} />
-                <DeleteList
-                  name={list.name}
-                  id={id}
-                  userId={session.user.id}
-                />
-              </div>
-            )}
-
-            <div className="highlight-nav list-styles">
-              <Link href={`/lists/${list.id}/`}>
-                <Grid size={20} />
-              </Link>
-              <Link href={`/lists/${list.id}/catalogue`}>
-                <List size={20} />
-              </Link>
-              <Link href={`/lists/${list.id}/grid`}>
-                <Table size={20} />
-              </Link>
-            </div>
-          </div>
-        }
-      >
-        <MovieCatalogue movies={movies} />
-      </Section>
-    </main>
-  );
+  return <MovieCatalogue movies={movies} />;
 }
--- src/app/lists/[listId]/page.tsx
+++ src/app/lists/[listId]/page.tsx
@@ -1,17 +1,8 @@
-import Link from "next/link";
 import { notFound } from "next/navigation";
-import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/authOptions";
-import { List, Grid, Table } from "react-feather";
-
-import { Section } from "@/components/section";
 import { getList } from "@/services/listService";
-import { DeleteList } from "./deleteList";
-import { RenameList } from "./renameList";
 import { MovieList } from "@/components/movieList";
 
 export default async function ListPage({ params }: { params: { listId: string } }) {
-  const session = await getServerSession(authOptions);
   const id = Number(params.listId);
 
   const list = await getList(id);
@@ -23,44 +14,8 @@ export default async function ListPage({ params }: { params: { listId: string } 
   const movies = list.listMovies.map(movie => movie.movie);
 
   return (
-    <main className="list">
-      <Section
-        header={
-          <div className="header">
-            <div className="list-title">
-              <h2 className="yellow-name-header h3">
-                <Link href={"/users/" + list.userId}>{list.user.username}</Link>&rsquo;s {list.name}
-              </h2>
-              <p className="movie-amount">{movies.length}</p>
-            </div>
-
-            {session && session.user.id === list.userId && (
-              <div className="list-edit">
-                <RenameList id={id} />
-                <DeleteList
-                  name={list.name}
-                  id={id}
-                  userId={session.user.id}
-                />
-              </div>
-            )}
-
-            <div className="highlight-nav list-styles">
-              <Link href={`/lists/${list.id}/`}>
-                <Grid size={20} />
-              </Link>
-              <Link href={`/lists/${list.id}/catalogue`}>
-                <List size={20} />
-              </Link>
-              <Link href={`/lists/${list.id}/grid`}>
-                <Table size={20} />
-              </Link>
-            </div>
-          </div>
-        }
-      >
-        <MovieList movies={movies} />
-      </Section>
-    </main>
+    <>
+      <MovieList movies={movies} />
+    </>
   );
 }

0c0a8fed71872594cc10765e53b391fad1cdf50b
Katariina Ruotsalainen <bkruotsalainen@gmail.com>
2024-05-22 10:38:15 +0300

    Move formAction function to renameListForm

--- src/app/lists/[listId]/renameList.tsx
+++ src/app/lists/[listId]/renameList.tsx
@@ -2,35 +2,15 @@
 
 import { useState } from "react";
 import { Edit } from "react-feather";
-import { toast } from "react-toastify";
 
 import Modal from "@/components/modal";
 import { RenameListForm } from "./renameListForm";
-import { updateListName } from "@/services/listService";
 
 export const RenameList = ({ id }: { id: number }) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
   const closeModal = () => {
     setIsOpen(false);
-  };
-
-  const renameList = async (formData: FormData) => {
-    const name = formData.get("name");
-
-    if (name) {
-      await updateListName(id, name.toString());
-      toast(
-        <p>
-          List was renamed to <span className="highlight-text">{name.toString()}</span>
-        </p>,
-        {
-          icon: <Edit strokeWidth={2.5} />,
-          className: "yellow-toast",
-        },
-      );
-      setIsOpen(false);
-    }
   };
 
   return (
@@ -46,7 +26,12 @@ export const RenameList = ({ id }: { id: number }) => {
       </button>
 
       <Modal
-        content={<RenameListForm formAction={renameList} />}
+        content={
+          <RenameListForm
+            closeModal={closeModal}
+            id={id}
+          />
+        }
         isOpen={isOpen}
         closeModal={closeModal}
       />
--- src/app/lists/[listId]/renameListForm.tsx
+++ src/app/lists/[listId]/renameListForm.tsx
@@ -1,29 +1,51 @@
-type Props = {
-  formAction: (formData: FormData) => void;
-};
+import { Edit } from "react-feather";
+import { toast } from "react-toastify";
 
-export const RenameListForm = ({ formAction }: Props) => (
-  <form
-    className="form"
-    action={formAction}
-  >
-    <label
-      className="h4"
-      htmlFor="name"
+import { updateListName } from "@/services/listService";
+
+export const RenameListForm = ({ closeModal, id }: { closeModal: () => void; id: number }) => {
+  const renameList = async (formData: FormData) => {
+    const name = formData.get("name");
+
+    if (name) {
+      await updateListName(id, name.toString());
+      toast(
+        <p>
+          List was renamed to <span className="highlight-text">{name.toString()}</span>
+        </p>,
+        {
+          icon: <Edit strokeWidth={2.5} />,
+          className: "yellow-toast",
+        },
+      );
+
+      closeModal();
+    }
+  };
+
+  return (
+    <form
+      className="form"
+      action={renameList}
     >
-      Rename list
-    </label>
-    <input
-      type="text"
-      name="name"
-      placeholder="Give list a new name..."
-      required
-    />
-    <button
-      className="list-form-button"
-      type="submit"
-    >
-      Rename
-    </button>
-  </form>
-);
+      <label
+        className="h4"
+        htmlFor="name"
+      >
+        Rename list
+      </label>
+      <input
+        type="text"
+        name="name"
+        placeholder="Give list a new name..."
+        required
+      />
+      <button
+        className="list-form-button"
+        type="submit"
+      >
+        Rename
+      </button>
+    </form>
+  );
+};

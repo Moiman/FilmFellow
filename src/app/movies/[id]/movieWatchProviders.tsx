2fe34897595943b0b351082e5982829983e3abe0
Katariina Ruotsalainen <bkruotsalainen@gmail.com>
2024-05-23 09:53:32 +0300

    Add JustWatch logo and fix type

--- src/app/movies/[id]/movieWatchProviders.tsx
+++ src/app/movies/[id]/movieWatchProviders.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
+import Link from "next/link";
 
-import { getWatchProvidersByMovieId } from "@/services/movieService";
+import { type getWatchProvidersByMovieId } from "@/services/movieService";
 
 export const MovieWatchProviders = ({
   watchProviders,
@@ -11,17 +12,35 @@ export const MovieWatchProviders = ({
     <div className="movie-streaming-sites">
       {watchProviders.length > 0 && (
         <>
-          <p>Watch at:</p>
-          {watchProviders.map(provider => (
-            <Image
-              key={provider.provider_id}
-              src={`https://image.tmdb.org/t/p/w45/${provider.logo_path}`}
-              width={25}
-              height={25}
-              title={provider.provider_name}
-              alt={provider.provider_name}
-            />
-          ))}
+          <div className="centered-logos">
+            <p>Watch at</p>
+            {watchProviders.map(provider => (
+              <Image
+                key={provider.provider_id}
+                src={`https://image.tmdb.org/t/p/w45/${provider.logo_path}`}
+                width={25}
+                height={25}
+                title={provider.provider_name}
+                alt={provider.provider_name}
+              />
+            ))}
+          </div>
+
+          <div className="centered-logos">
+            <p>Provided by</p>
+            <Link
+              href="https://www.justwatch.com/"
+              style={{ display: "flex", alignItems: "center" }}
+            >
+              <Image
+                src="/logos/justwatch_logo.svg"
+                height={30}
+                width={80}
+                alt="JustWatch"
+                title="JustWatch"
+              />
+            </Link>
+          </div>
         </>
       )}
     </div>

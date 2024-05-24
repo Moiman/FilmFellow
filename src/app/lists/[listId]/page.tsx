226b7453148582f27c174f14b5cda40461878393
Harri Nieminen <moiman@posteo.net>
2024-05-24 10:13:30 +0300

    add watchlist and watched lists to user profile (#81)
    * add watchlist and watched lists to user profile
    
    * fix refresh watched
    
    * sort imports

--- src/app/lists/[listId]/page.tsx
+++ src/app/lists/[listId]/page.tsx
@@ -3,9 +3,7 @@ import { getList } from "@/services/listService";
 import { MovieList } from "@/components/movieList";
 
 export default async function ListPage({ params }: { params: { listId: string } }) {
-  const id = Number(params.listId);
-
-  const list = await getList(id);
+  const list = await getList(params.listId);
 
   if (!list) {
     notFound();

1e984c65c23b05b1e9b1f0938a3a8f53eb051942
Katariina Ruotsalainen <64400810+bkruotsalainen@users.noreply.github.com>
2024-05-27 13:14:35 +0300

    User description and social media (#82)
    * Simplify profile settings route
    
    * Replace facebook logo with tiktok
    
    * Fix tiktok logo
    
    * Separate description and social medias to own component
    
    * Add updating user description and social media
    
    * Hide not implemented sections in profile and profile settings
    
    * Adjust layout
    
    * Polish and fix toast style
    
    * Move title
    
    * Add first version of tests
    
    * Update tests
    
    * Fix tests
    
    * Clean users and session after tests
    
    * Delete user after test
    
    * Update description and social media form
    
    * Add tests for invalid inputs
    
    * Remove repeated length check
    
    * Polish tests
    
    * Remove revalidatePath
    
    * Import order
    
    * Move tiktok icon and update schema

---
+++ src/app/settings/profileSettings.tsx
@@ -0,0 +1,510 @@
+"use client";
+import { useState } from "react";
+
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+
+import { signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import type { User } from "next-auth";
+
+import { toast } from "react-toastify";
+import { Eye, EyeOff, Save } from "react-feather";
+
+import Modal from "@/components/modal";
+
+interface Props {
+  user: User;
+}
+
+interface UpdateUsernameFormValues {
+  username: string;
+}
+
+interface UpdateEmailFormValues {
+  email: string;
+}
+
+interface UpdatePasswordFormValues {
+  password: string;
+}
+
+const updateEmailSchema = yup.object({
+  email: yup.string().trim().required("Email is required").email("Must be a valid email"),
+});
+
+const updateUsernameSchema = yup.object({
+  username: yup
+    .string()
+    .trim()
+    .required("Username is required")
+    .min(2, "Username too short, minimum length is 2")
+    .max(50, "Username too long, max length is 50"),
+});
+
+const updatePasswordSchema = yup.object({
+  password: yup
+    .string()
+    .required("Password is required")
+    .min(6, "Password must be at least 6 characters long")
+    .matches(/^(?=.*[a-z])/, "Password requires atleast 1 regural character")
+    .matches(/^(?=.*[A-Z])/, "Password requires atleast 1 capital character")
+    .matches(/^(?=.*[0-9])/, "Password requires atleast 1 number")
+    .matches(/^(?=.*[!@#$%^&*])/, "Password requires atleast 1 special character"),
+});
+
+export const ProfileSettings = ({ user }: Props) => {
+  const [activeUsername, setActiveUsername] = useState(false);
+  const [activeEmail, setActiveEmail] = useState(false);
+  const [activePassword, setActivePassword] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const [error, setError] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+  const { update } = useSession();
+
+  const router = useRouter();
+
+  const handleDelete = async () => {
+    try {
+      const response = await fetch(`/api/users/delete`, {
+        method: "DELETE",
+      });
+
+      if (!response.ok) {
+        setError(response.statusText);
+        throw response.status;
+      }
+
+      const data = await response.json();
+
+      if (data.error) {
+        setError(data.error);
+        throw data.error;
+      }
+
+      setError("");
+      signOut();
+    } catch (error) {
+      console.error(error);
+      toast.error(<p>Something went wrong!</p>);
+      throw error;
+    }
+  };
+
+  const {
+    handleSubmit: handleEmailChange,
+    register: emailRegister,
+    reset: emailReset,
+    setError: setEmailError,
+    formState: { errors: errorsEmail },
+  } = useForm<UpdateEmailFormValues>({
+    defaultValues: {
+      email: user.email,
+    },
+    resolver: yupResolver(updateEmailSchema),
+  });
+
+  const {
+    handleSubmit: handleUsernameChange,
+    register: usernameRegister,
+    reset: usernameReset,
+    setError: setUsernameError,
+    formState: { errors: errorsUsername },
+  } = useForm<UpdateUsernameFormValues>({
+    defaultValues: {
+      username: user.username,
+    },
+    resolver: yupResolver(updateUsernameSchema),
+  });
+
+  const {
+    handleSubmit: handlePasswordChange,
+    register: passwordRegister,
+    reset: passwordReset,
+    formState: { errors: errorsPassword },
+  } = useForm<UpdatePasswordFormValues>({
+    defaultValues: {
+      password: "",
+    },
+    resolver: yupResolver(updatePasswordSchema),
+  });
+
+  const handleEmailSubmit = async (formData: UpdateEmailFormValues) => {
+    try {
+      const updatedEmail = {
+        email: formData.email,
+      };
+      if (updatedEmail.email === user.email) {
+        setEmailError("email", {
+          type: "manual",
+          message: "Can't change email to same as you already have",
+        });
+        return;
+      }
+      const response = await fetch(`/api/users/update`, {
+        method: "PUT",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updatedEmail),
+      });
+
+      const data = await response.json();
+
+      if (data.error) {
+        setError(data.error);
+        return;
+      }
+      if (response.ok && !data.error) {
+        await update(data);
+        setActiveEmail(false);
+        emailReset();
+        setError("");
+        router.refresh();
+        toast(<p>Your e-mail was changed!</p>, {
+          icon: <Save />,
+          className: "cyan-toast",
+        });
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error(<p>Something went wrong!</p>);
+    }
+  };
+
+  const handleUsernameSubmit = async (formData: UpdateUsernameFormValues) => {
+    try {
+      const updatedUsername = {
+        username: formData.username,
+      };
+      if (updatedUsername.username === user.username) {
+        setUsernameError("username", {
+          type: "manual",
+          message: "Can't change username to same as you already have",
+        });
+        return;
+      }
+      const response = await fetch(`/api/users/update`, {
+        method: "PUT",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updatedUsername),
+      });
+
+      const data = await response.json();
+
+      if (data.error) {
+        setError(data.error);
+        return;
+      }
+      if (response.ok && !data.error) {
+        await update(data);
+        setActiveUsername(false);
+        usernameReset();
+        setError("");
+        router.refresh();
+        toast(<p>Your username was changed!</p>, {
+          icon: <Save />,
+          className: "cyan-toast",
+        });
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error(<p>Something went wrong!</p>);
+    }
+  };
+
+  const handlePasswordSubmit = async (formData: UpdatePasswordFormValues) => {
+    try {
+      const updatedPassword = {
+        password: formData.password,
+      };
+      const response = await fetch(`/api/users/update`, {
+        method: "PUT",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updatedPassword),
+      });
+
+      const data = await response.json();
+
+      if (data.error) {
+        setError(data.error);
+        return;
+      }
+      if (response.ok && !data.error) {
+        await update(data);
+        setActivePassword(false);
+        passwordReset();
+        setError("");
+        router.refresh();
+        toast(<p>Your password was changed!</p>, {
+          icon: <Save />,
+          className: "cyan-toast",
+        });
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error(<p>Something went wrong!</p>);
+    }
+  };
+
+  const resetAllFields = () => {
+    setError("");
+    passwordReset();
+    emailReset();
+    usernameReset();
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+    setError("");
+  };
+
+  return (
+    <div className="profile-settings-right">
+      <div className="profile-settings-divider">
+        <h3>User Information</h3>
+
+        <p className="description">Here you can change your username, email or password.</p>
+
+        {activeUsername ? (
+          <form
+            onSubmit={handleUsernameChange(handleUsernameSubmit)}
+            className="profile-settings-wrapper"
+          >
+            <label htmlFor="username">Username</label>
+            <div className="profile-settings-input">
+              <input
+                id="username"
+                type="text"
+                {...usernameRegister("username")}
+                required
+              />
+
+              <button type="submit">Save</button>
+            </div>
+            <div>
+              {error && <p className="error-text">{error}</p>}
+              {errorsUsername.username && <p className="error-text">{errorsUsername.username.message}</p>}
+            </div>
+          </form>
+        ) : (
+          <div className="profile-settings-wrapper">
+            <label>Username</label>
+            <div className="profile-settings-input">
+              <p>{user.username}</p>
+              <button
+                onClick={() => {
+                  setActiveUsername(!activeUsername);
+                  setActiveEmail(false);
+                  setActivePassword(false);
+                  resetAllFields();
+                }}
+              >
+                Edit
+              </button>
+            </div>
+          </div>
+        )}
+        {activeEmail ? (
+          <form
+            onSubmit={handleEmailChange(handleEmailSubmit)}
+            className="profile-settings-wrapper"
+          >
+            <label htmlFor="email">Email</label>
+            <div className="profile-settings-input">
+              <input
+                id="email"
+                type="email"
+                {...emailRegister("email")}
+                required
+              />
+
+              <button type="submit">Save</button>
+            </div>
+            <div>
+              {error && <p className="error-text">{error}</p>}
+              {errorsEmail.email && <p className="error-text">{errorsEmail.email.message}</p>}
+            </div>
+          </form>
+        ) : (
+          <div className="profile-settings-wrapper">
+            <label>Email</label>
+
+            <div className="profile-settings-input">
+              <p>{user.email}</p>
+              <button
+                onClick={() => {
+                  setActiveUsername(false);
+                  setActiveEmail(!activeEmail);
+                  setActivePassword(false);
+                  resetAllFields();
+                }}
+              >
+                Edit
+              </button>
+            </div>
+          </div>
+        )}
+        {activePassword ? (
+          <form
+            onSubmit={handlePasswordChange(handlePasswordSubmit)}
+            className="profile-settings-wrapper"
+          >
+            <label htmlFor="password">Password</label>
+            <div className="profile-settings-input">
+              <div className="password-input-wrapper">
+                <input
+                  id="password"
+                  type={showPassword ? "text" : "password"}
+                  {...passwordRegister("password")}
+                  required
+                  placeholder="Set new password"
+                />
+                <button
+                  className="form-group-icon button-transparent"
+                  type="button"
+                  onClick={() => setShowPassword(!showPassword)}
+                  tabIndex={-1}
+                >
+                  {showPassword ? <Eye size={20} /> : <EyeOff size={20} />}
+                </button>
+              </div>
+              <button type="submit">Save</button>
+            </div>
+            <div>
+              {error && <p className="error-text">{error}</p>}
+              {errorsPassword.password && <p className="error-text">{errorsPassword.password.message}</p>}
+            </div>
+          </form>
+        ) : (
+          <div className="profile-settings-wrapper">
+            <label>Password</label>
+
+            <div className="profile-settings-input">
+              <p>************</p>
+              <button
+                onClick={() => {
+                  setActiveUsername(false);
+                  setActiveEmail(false);
+                  setActivePassword(!activePassword);
+                  resetAllFields();
+                }}
+              >
+                Edit
+              </button>
+            </div>
+          </div>
+        )}
+        <div className="profile-settings-wrapper">
+          <label>Delete Account</label>
+          <div className="profile-settings-input">
+            <p>Permanently delete your account.</p>
+            <button
+              className="button-pink"
+              onClick={() => setIsOpen(true)}
+            >
+              Delete
+            </button>
+          </div>
+          <Modal
+            isOpen={isOpen}
+            closeModal={closeModal}
+            content={
+              <div style={{ display: "grid", textAlign: "center" }}>
+                <div>
+                  <h3 className="h4">Are you sure you want to delete your account?</h3>
+
+                  <p className="description">
+                    If you delete your account, all your lists, reviews and other data will be destroyed permanently.
+                  </p>
+
+                  {error && (
+                    <p
+                      className="error-text"
+                      style={{ display: "flex", justifyContent: "center", padding: "5px" }}
+                    >
+                      {error}
+                    </p>
+                  )}
+                </div>
+                <div className="modal-buttons">
+                  <button
+                    onClick={() => {
+                      setIsOpen(false);
+                    }}
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    className="button-pink"
+                    onClick={handleDelete}
+                  >
+                    Delete Account
+                  </button>
+                </div>
+              </div>
+            }
+          />
+        </div>
+      </div>
+      {/* Hide until implemented
+      <div className="profile-settings-divider">
+        <h3>Profile Privacy</h3>
+        <p className="description">Here you can choose who can see your profile.</p>
+        <form>
+          <label className="label-row">
+            <input
+              type="radio"
+              name="selectOne"
+            />
+            Everyone
+          </label>
+          <label className="label-row">
+            <input
+              type="radio"
+              name="selectOne"
+            />
+            All users
+          </label>
+          <label className="label-row">
+            <input
+              type="radio"
+              name="selectOne"
+            />
+            Users you follow
+          </label>
+          <label className="label-row">
+            <input
+              type="radio"
+              name="selectOne"
+            />
+            Only me
+          </label>
+        </form>
+      </div>
+      <div className="profile-settings-divider">
+        <h3>Content Filter</h3>
+        <p className="description">
+          Here you can choose to filter content and make your movie browsing experience more suitable for your needs.
+        </p>
+        <form>
+          <label className="label-row">
+            <input type="checkbox" /> Violence
+          </label>
+          <label className="label-row">
+            <input type="checkbox" /> Lorem Ipsum
+          </label>
+        </form>
+      </div>
+          */}
+    </div>
+  );
+};
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,3 +1,5 @@
+"use server";
+
 import { authOptions } from "@/authOptions";
 import prisma from "@/db";
 import { Role } from "@prisma/client";
@@ -140,6 +142,46 @@ const changeUserStatusById = async (id: number, status: boolean, banDuration?: n
   }
 };
 
+const getDescriptionAndSocialMedia = async (userId: number) => {
+  return await prisma.users.findUnique({
+    where: {
+      id: userId,
+    },
+    select: {
+      username: true,
+      isActive: true,
+      description: true,
+      twitter: true,
+      instagram: true,
+      tiktok: true,
+    },
+  });
+};
+
+const updateDescriptionAndSocialMedia = async (
+  userId: number,
+  description: string,
+  twitter: string,
+  instagram: string,
+  tiktok: string,
+) => {
+  const session = await getServerSession(authOptions);
+
+  if (userId === session?.user.id) {
+    await prisma.users.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        description: description,
+        twitter: twitter,
+        instagram: instagram,
+        tiktok: tiktok,
+      },
+    });
+  }
+};
+
 export {
   createUser,
   findUserByEmail,
@@ -150,4 +192,6 @@ export {
   getAllUsers,
   updateUserLastVisited,
   changeUserStatusById,
+  getDescriptionAndSocialMedia,
+  updateDescriptionAndSocialMedia,
 };

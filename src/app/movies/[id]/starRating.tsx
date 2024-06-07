20a8f6780a295bc61a08ed24c2df84891c003a35
Katariina Ruotsalainen <bkruotsalainen@gmail.com>
2024-06-07 12:38:38 +0300

    Add hover effect for StarRating

--- src/app/movies/[id]/starRating.tsx
+++ src/app/movies/[id]/starRating.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Star } from "react-feather";
 
 interface Props {
@@ -7,18 +8,37 @@ interface Props {
 }
 
 export const StarRating = ({ rating, setRating, size = 30 }: Props) => {
+  const [hoverRating, setHoverRating] = useState<number | null>(null);
+  const [currentRating, setCurrentRating] = useState<number | null>(rating);
+
+  const handleMouseOver = (starRating: number) => {
+    setCurrentRating(null);
+    setHoverRating(starRating);
+  };
+
+  const handleMouseOut = () => {
+    setCurrentRating(rating);
+    setHoverRating(null);
+  };
+
   return (
     <div className="star-rating">
       {[1, 2, 3, 4, 5].map(starRating => (
         <button
           key={starRating}
           onClick={() => setRating(starRating)}
+          onMouseOver={() => handleMouseOver(starRating)}
+          onMouseOut={handleMouseOut}
           className="star-rating-button"
           aria-label={`Rate ${starRating} of 5 stars`}
           aria-pressed={rating === starRating ? "true" : "false"}
         >
           <Star
-            className={rating && rating >= starRating ? "selected" : "not-selected"}
+            className={
+              (hoverRating && hoverRating >= starRating) || (currentRating !== null && currentRating >= starRating)
+                ? "selected"
+                : "not-selected"
+            }
             strokeWidth={2}
             size={size}
           />

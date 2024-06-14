07ac237d01e9ec85863f9411e661e74cfdfdcd58
Katariina Ruotsalainen <bkruotsalainen@gmail.com>
2024-06-14 15:39:51 +0300

    Move recommendations

--- src/services/isRecommendations.ts
+++ src/services/isRecommendations.ts
@@ -2,10 +2,10 @@ import { getUserOwnFavoritesAndRatings } from "@/services/watchedService";
 import { getUserRecommendations } from "./getUserRecommendations";
 
 export const isRecommendations = async () => {
-  const recommendations = await getUserRecommendations(undefined, 36);
-
   try {
     const ratingsFavourites = await getUserOwnFavoritesAndRatings();
+    const recommendations = await getUserRecommendations(undefined, 36);
+
     if (
       (ratingsFavourites.ratings.length === 0 && ratingsFavourites.favorites.length === 0) ||
       recommendations.length === 0

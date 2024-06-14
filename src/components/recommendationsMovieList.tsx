328beb9eeb7788ea8379c5b232b46b18713e1540
Katariina Ruotsalainen <bkruotsalainen@gmail.com>
2024-06-14 10:14:21 +0300

    Show popular movies when there's nothing to recommend

--- src/components/recommendationsMovieList.tsx
+++ src/components/recommendationsMovieList.tsx
@@ -1,6 +1,7 @@
 import { MovieList } from "@/components/movieList";
 import { getMovieRecommendations } from "@/services/getMovieRecommendations";
 import { getUserRecommendations } from "@/services/getUserRecommendations";
+import { getMovieByLimitTypeGenre } from "@/services/movieService";
 
 type Props = {
   limit: number;
@@ -13,14 +14,25 @@ export default async function RecommendationsMovieList({ limit, selectedGenre, m
     ? await getMovieRecommendations(movieId, limit)
     : await getUserRecommendations(selectedGenre, limit);
 
+  if (!movieId && recommendationMovies.length === 0) {
+    return (
+      <>
+        <div style={{ textAlign: "center", margin: "40px 0" }}>
+          <h3 className="h5">
+            Add movies to favorites or leave reviews to get <span className="yellow">personalized suggestions</span>!
+          </h3>
+          <h4 className="h6">Here&rsquo;s some popular movies you might enjoy:</h4>
+        </div>
+
+        <MovieList movies={await getMovieByLimitTypeGenre(36, "popular", selectedGenre)} />
+      </>
+    );
+  }
+
   return (
     <MovieList
       movies={recommendationMovies}
-      emptyText={
-        movieId
-          ? "No matches for similar movies. Browse our collection to find more films you'll like."
-          : "Add your favorite movies or leave reviews to get personalized suggestions!"
-      }
+      emptyText="No matches for similar movies. Browse our collection to find more films you'll like."
     />
   );
 }

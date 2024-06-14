9fb3404790b6cb5589baabb6de25eb83dbd41f54
Katariina Ruotsalainen <bkruotsalainen@gmail.com>
2024-06-14 15:08:38 +0300

    Check recommendations length

--- src/services/isRecommendations.ts
+++ src/services/isRecommendations.ts
@@ -1,9 +1,15 @@
 import { getUserOwnFavoritesAndRatings } from "@/services/watchedService";
+import { getUserRecommendations } from "./getUserRecommendations";
 
 export const isRecommendations = async () => {
+  const recommendations = await getUserRecommendations(undefined, 36);
+
   try {
     const ratingsFavourites = await getUserOwnFavoritesAndRatings();
-    if (ratingsFavourites.ratings.length === 0 && ratingsFavourites.favorites.length === 0) {
+    if (
+      (ratingsFavourites.ratings.length === 0 && ratingsFavourites.favorites.length === 0) ||
+      recommendations.length === 0
+    ) {
       return false;
     } else {
       return true;

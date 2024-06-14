25bcc650e7811f5951859fd05b5b5ee28ef8060c
Harri Nieminen <moiman@posteo.net>
2024-06-14 14:12:40 +0300

    Statistics

--- src/app/admin/statistics/adminPanelStatistics.tsx
+++ src/app/admin/statistics/adminPanelStatistics.tsx
@@ -1,5 +1,24 @@
-"use client";
+import { Section } from "@/components/section";
+import { statistics } from "@/services/adminStaticsService";
 
-export const AdminPanelStatistics = () => {
-  return <div>In Progress</div>;
+export const AdminPanelStatistics = async () => {
+  const stats = await statistics();
+  return (
+    <div className="admin-panel-content">
+      <Section header={<h2>Statistics</h2>}>
+        <ul>
+          <li>Users: {stats.users}</li>
+          <li>Favorites: {stats.favorites}</li>
+          <li>Watched: {stats.watchedRatings}</li>
+          <li>Ratings: {stats.ratings}</li>
+          <li>Lists: {stats.lists}</li>
+          <li>Reports: {stats.reports}</li>
+          <li>Movies: {stats.movies}</li>
+          <li>Persons: {stats.persons}</li>
+          <li>Reviews: {stats.reviews}</li>
+          <li>Imported reviews: {stats.importedReviews}</li>
+        </ul>
+      </Section>
+    </div>
+  );
 };
